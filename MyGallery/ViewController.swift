3b75e56619a4803ba2bf85a5b1ae76ab3ec0755f
Valery Vasilevich <valery.vasilevich@innowise-group.com>
2022-01-12 22:15:43 +0300

    Initial Commit

---
+++ MyGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGallery
+//
+//  Created by Valera Vasilevich on 12.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
